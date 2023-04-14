72642ce659023d14221345da156b0cdfe5605a1b
redowanhossain <redowan.bu.cse@gmail.com>
2023-04-14 17:06:45 +0600

    this time organized admin home page by mastering

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.layouts.master');
+        return view('backend.pages.dashboard.index');
     }
 }
---
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- footer area start-->
+<footer>
+            <div class="footer-area">
+                <p>© Copyright 2023. All right reserved. Modified by <a href="https://colorlib.com/wp/">Redowan</a>.</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
